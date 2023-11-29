3d8d3fb89c267d053ffbad88986e31a327453b50
arunprasadh7 <m.arunprasadh96@gmail.com>
2023-11-29 20:21:02 +0530

    Operators

---
+++ 3 Operators/script.js
@@ -0,0 +1,55 @@
+// Operators
+
+// calculate the age 
+let birthYear = 1996
+
+age = (2023 - birthYear)
+console.log(age)
+
+// 1 Arithmetic operators
+// =, -, *, /, **
+
+let number1 = 30
+let number2 = 20
+
+console.log(number1 + number2)
+console.log(number1 - number2)
+console.log(number1 * number2)
+console.log(number1 / number2)
+
+
+// 2 Assignment operators
+// =, ==, ===
+
+// 3 Comparison operators
+// >, >=, <, <=, !=
+
+let num1 = 30;
+let num2 = 60;
+
+console.log(num1 > num2)
+console.log(num1 < num2)
+console.log(num1 == num2)
+console.log(num1 != num2)
+
+let voterAge = 18
+let personAge = 16
+personAge = 25
+
+console.log(personAge >= voterAge)
+
+
+console.log(2**3)
+
+const fname = 'Arun'
+const lname = 'Prasadh'
+
+console.log(fname + ' ' + lname)
+// console.log(fname - ' ' - lname)
+
+
+// Operators Precedence 
+
+const currenYear = 2025
+
+console.log(currenYear - 1991 > currenYear - 2001)
