b3854e3e4c112c8e87966995760659f2e1881b2b
arunprasadh7 <m.arunprasadh96@gmail.com>
2023-11-29 10:43:28 +0530

    Basics

---
+++ 1 Starting Variables/script.js
@@ -0,0 +1,34 @@
+let myFirstJob = 'Mechanical Engineer';
+let mySecondJob = 'Fullstack Developer';
+console.log(myFirstJob);
+console.log(mySecondJob);
+
+//this is a comment
+
+/*this
+is a multi 
+line 
+comment */
+
+console.log(typeof(true))
+console.log(typeof('Arun'))
+console.log(typeof(30))
+console.log(typeof(25.5))
+
+let suriname;
+console.log(typeof(suriname));
+
+let pariname = null;
+console.log(typeof(pariname))
+
+let firstName = 'Jack';
+firstName = 'Captain Jack Sparrow'
+console.log(firstName)
+
+let age = 'Twenty seven';
+age = 27
+console.log(age)
+console.log(typeof age)
+
+let myName;
+console.log(typeof myName)
---
+++ 2 Let const var/script.js
@@ -0,0 +1,25 @@
+// there are 3 ways to declare variables in JS - let, var, const
+
+// let values can be updated in the future
+let age;
+age = 10;
+age = 20;
+
+console.log(age)
+
+// const - values cannot be changed or updated 
+const birthMonth = 'August';
+
+console.log(birthMonth)
+
+// var - similar to let but older version
+var myName = 'Arun';
+myName = 'John Wick';
+
+console.log(myName)
+
+/* const is immutable
+  var is mutable - older version / but they are function scoped
+  let is also mutable - newer version
+  */
+
