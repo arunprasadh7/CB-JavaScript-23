a528272443f412fa9ac201de6801ff93004ecf5d
arunprasadh7 <m.arunprasadh96@gmail.com>
2023-11-29 21:06:13 +0530

    if else basics

---
+++ 5 if else/script.js
@@ -0,0 +1,11 @@
+// If else decision block
+
+const age = 17
+
+if (age >= 18) {
+  console.log('You are eligible to vote.')
+}
+
+else{
+  console.log(`The person is not an adult. Wait for ${18-age} year(s).`)
+}
