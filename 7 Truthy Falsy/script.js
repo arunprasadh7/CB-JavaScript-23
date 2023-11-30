549d557de320e4eaf538bd43f7b8c70abc97cf5f
arunprasadh7 <m.arunprasadh96@gmail.com>
2023-11-30 09:38:28 +0530

    Truthy falsy

---
+++ 7 Truthy Falsy/script.js
@@ -0,0 +1,38 @@
+// Truthy Falsy values 
+
+// falsy values -- 0, '', null, undefined, NaN 
+
+console.log(Boolean(0))
+console.log(Boolean(1))
+console.log(Boolean(10))
+console.log(Boolean(-10))
+console.log(Boolean(-0))
+console.log(Boolean(''))
+console.log(Boolean('Arun'))
+console.log(Boolean({}))
+console.log(Boolean(null))
+console.log(Boolean(undefined))
+console.log(Boolean(NaN))
+
+// if else example 
+let salary
+// salary = 50
+
+if (salary){
+  console.log('Enjoy your salary.')
+}
+else {
+  console.log('Hope you get paid soon.')
+}
+
+// eg 2
+
+let height
+// height = 20
+
+if (height){
+  console.log('Height is defined.')
+}
+else {
+  console.log('Height is not defined.')
+}
